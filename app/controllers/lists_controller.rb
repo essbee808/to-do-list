6a100d7925794fc61f5d0e8c71f275e10b71b70b
essbee808 <sheena.myb@gmail.com>
2021-03-11 20:32:17 -0800

    add show method to lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,17 @@
 class ListsController < ApplicationController
 
     def index
+        @list = List.new
         @lists = List.all
         
         #render 'lists/index.html.erb'
+    end
+
+    def show 
+        #I need to load the list
+        #Which list?
+        #params has all the data passed by the user
+        @list = List.find(params[:id])
     end
 
 end
