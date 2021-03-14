02cdd9b5e1dc5c802b495b277da77885469970ad
essbee808 <sheena.myb@gmail.com>
2021-03-13 19:36:49 -0800

    Create and Retrieve and List Lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,4 +14,27 @@ class ListsController < ApplicationController
         @list = List.find(params[:id])
     end
 
+    def create
+        # params opens up the envelope the user sends you when they submit HTTP requests
+        
+        #raise list_params.inspect
+        #instantiate a new list object
+        @list = List.new(list_params)
+
+        #take the data from the form using the naming convention
+        @list.name = params[:list][:name]
+
+        #insert into database
+        @list.save
+
+        # tell browser to redirect to show action and find the ID of this list object
+        redirect_to list_path(@list)
+    end
+
+    private
+    def list_params
+        # sanitize and explicitly permit the name key in the lift key of my params hash
+        params.require(:list).permit(:name)
+    end
+
 end
